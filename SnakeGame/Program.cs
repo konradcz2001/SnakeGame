c62b761be0e146a5592243527bd2b07f98ae629f
Konrad Czardybon <72301741+konradcz2001@users.noreply.github.com>
2023-11-27 18:48:46 +0100

    Update Program.cs
    Implementacja w Mainie

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -8,7 +8,46 @@ public static class Program
 {
     public static async Task Main(string[] args)
     {
+        var tickRate = TimeSpan.FromMilliseconds(100);
+        var snakeGame = new SnakeGame();
 
+        using (var cts = new CancellationTokenSource())
+        {
+            async Task MonitorKeyPresses()
+            {
+                while (!cts.Token.IsCancellationRequested)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var key = Console.ReadKey(intercept: true).Key;
+                        snakeGame.OnKeyPress(key);
+                    }
+
+                    await Task.Delay(10);
+                }
+            }
+
+            var monitorKeyPresses = MonitorKeyPresses();
+
+            do
+            {
+                snakeGame.OnGameTick();
+                snakeGame.Render();
+                await Task.Delay(tickRate);
+            } while (!snakeGame.GameOver);
+
+            // Allow time for the user to weep before the application exits.
+            for (var i = 0; i < 3; i++)
+            {
+                Console.Clear();
+                await Task.Delay(500);
+                snakeGame.Render();
+                await Task.Delay(500);
+            }
+
+            cts.Cancel();
+            await monitorKeyPresses;
+        }
     }
 }
 

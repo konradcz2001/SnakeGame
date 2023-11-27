2495b8845b6db39471b7fcfdd6752356a7f78fc4
Konrad Czardybon <72301741+konradcz2001@users.noreply.github.com>
2023-11-27 18:39:27 +0100

    Update Program.cs
    Dodanie reprezentacji ruchu węża

--- SnakeGame/Program.cs
+++ SnakeGame/Program.cs
@@ -54,3 +54,89 @@ class Apple : IRenderable
         Console.Write("A");
     }
 }
+
+
+class Snake : IRenderable
+{
+    private List<Position> _body;
+    private int _growthSpurtsRemaining;
+
+    public Snake(Position spawnLocation, int initialSize = 1)
+    {
+        _body = new List<Position> { spawnLocation };
+        _growthSpurtsRemaining = Math.Max(0, initialSize - 1);
+        Dead = false;
+    }
+
+    public bool Dead { get; set; }
+    public Position Head => _body.First();
+    public IEnumerable<Position> Body => _body.Skip(1);
+
+    public void Move(Direction direction)
+    {
+        if (Dead) throw new InvalidOperationException();
+
+        Position newHead;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                newHead = Head.DownBy(-1);
+                break;
+
+            case Direction.Left:
+                newHead = Head.RightBy(-1);
+                break;
+
+            case Direction.Down:
+                newHead = Head.DownBy(1);
+                break;
+
+            case Direction.Right:
+                newHead = Head.RightBy(1);
+                break;
+
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+
+        if (_body.Contains(newHead) || !PositionIsValid(newHead))
+        {
+            Dead = true;
+            return;
+        }
+
+        _body.Insert(0, newHead);
+
+        if (_growthSpurtsRemaining > 0)
+        {
+            _growthSpurtsRemaining--;
+        }
+        else
+        {
+            _body.RemoveAt(_body.Count - 1);
+        }
+    }
+
+    public void Grow()
+    {
+        if (Dead) throw new InvalidOperationException();
+
+        _growthSpurtsRemaining++;
+    }
+
+    public void Render()
+    {
+        Console.SetCursorPosition(Head.Left, Head.Top);
+        Console.Write("X");
+
+        foreach (var position in Body)
+        {
+            Console.SetCursorPosition(position.Left, position.Top);
+            Console.Write("■");
+        }
+    }
+
+    private static bool PositionIsValid(Position position) =>
+        position.Top >= 0 && position.Left >= 0;
+}
